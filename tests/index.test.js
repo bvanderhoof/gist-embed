5268d983b745a74d77f275898781d16051833c29
Blair Vanderhoof <vanderx@gmail.com>
2019-03-10 09:02:07 -0700

    Update more tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -45,3 +45,32 @@ test('expect getAllGistEmbedDOMNodes to return a node ', () => {
 test('expect getAllGistEmbedDOMNodes to not return nodes', () => {
   expect(getFN('getAllGistEmbedDOMNodes')().length).toEqual(0);
 });
+
+test('generateJSONPCallbackPrefix returns incremented values', () => {
+  const generateJSONPCallbackPrefix = getFN('generateJSONPCallbackPrefix');
+  expect(generateJSONPCallbackPrefix()).toEqual('_gistEmbedJSONP_1');
+  expect(generateJSONPCallbackPrefix()).toEqual('_gistEmbedJSONP_2');
+});
+
+test('appendStylesheet appends if not exists', () => {
+  const appendStylesheet = getFN('appendStylesheet');
+  const mainCSSURL = 'https://www.github.com/main.css';
+  const main2CSSURL = 'https://www.github.com/main2.css';
+
+  // First time append adds it
+  appendStylesheet(mainCSSURL);
+  let linkTags = document.querySelectorAll('link');
+  expect(linkTags.length).toEqual(1);
+  expect(linkTags[0].getAttribute('href')).toEqual(mainCSSURL);
+
+  // Second time append same url doesn't add it
+  appendStylesheet(mainCSSURL);
+  linkTags = document.querySelectorAll('link');
+  expect(linkTags.length).toEqual(1);
+
+  // Third time append different url adds it
+  appendStylesheet(main2CSSURL);
+  linkTags = document.querySelectorAll('link');
+  expect(linkTags[1].getAttribute('href')).toEqual(main2CSSURL);
+  expect(linkTags.length).toEqual(2);
+});
